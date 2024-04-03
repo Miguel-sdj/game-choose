fe73e74f5a0613f4d3820c211fd1ed3b05430e35
luizfpbp <luizfpbp@gmail.com>
2024-04-03 15:50:18 -0300

    Fiz um timer e umas para no js fds

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
+const logList = []; // Array of the results
 
-function printLogging(choosen, value, status){
-    // document.add
-    console.log("Você escolheu o número: " + value + " e o número sorteado foi: " + choosen + " logo você " + status);
+const points = 0; // Players points
+
+let timer = 30;
+
+function printLogging(choosen, value, status) {
+  // document.add
+  console.log(
+    "Você escolheu o número: " +
+      value +
+      " e o número sorteado foi: " +
+      choosen +
+      " logo você " +
+      status
+  );
+
+  // Add the result of the round
+  logList.push(status);
 }
 
-function chooseWinner(value){
-    var choosen = Math.floor(Math.random() * 9) + 1;
-    var status
-    console.log(choosen)
-    if (choosen == value){
-        status = "Ganhou"
-    } else {
-        status = "Perdeu"
+function chooseWinner(value) {
+  var choosen = Math.floor(Math.random() * 9) + 1;
+  var status;
+  console.log(choosen);
+
+  if (choosen == value) {
+    status = "Ganhou";
+    points = points + 1000000;
+  } else {
+    status = "Perdeu";
+    points = 0;
+  }
+
+  printLogging(choosen, value, status);
+}
+
+function gameTimer() {
+    document.getElementById('timer').innerText = timer;
+    
+    timer--;
+    if (timer < 0) {
+        clearInterval(interval);
+        document.getElementById('timer').innerText = 'Vapo papai!!';
     }
 
-    printLogging(choosen, value, status)
 }
+
+let interval = setInterval(gameTimer, 1000);
