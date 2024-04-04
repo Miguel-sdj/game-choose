c31e23193171bf705a62d7daeb29018cbbac5fc1
luizfpbp <luizfpbp@gmail.com>
2024-04-04 09:02:48 -0300

    Fixed timer button relation

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ let points = 0; // Players points
 
 let timer = 5;
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 function printLogging(choosen, value, status) {
-
   let list = document.getElementById("logging");
   let item = document.createElement("li");
-  item.appendChild(document.createTextNode("Você escolheu o número: " + value + " e o número sorteado foi: " + choosen + " logo você " + status));
+  item.appendChild(
+    document.createTextNode(
+      "Você escolheu o número: " +
+        value +
+        " e o número sorteado foi: " +
+        choosen +
+        " logo você " +
+        status
+    )
+  );
   list.appendChild(item);
 
   // Add the result of the round
@@ -35,14 +41,17 @@ function chooseWinner(value) {
   printLogging(choosen, value, status);
 }
 
-function startTimer() {
+async function startTimer() {
+  document.getElementById("button").disabled = true;
+  document.getElementById("timer").innerText = "BO JOGAR????";
 
   while (timer >= 0) {
     document.getElementById("timer").innerText = timer;
     timer = timer - 1;
-    console.log()
+    await sleep(1000);
   }
 
   document.getElementById("timer").innerText = "Vapo papai!!";
   timer = 5;
+  document.getElementById("button").disabled = false;
 }
